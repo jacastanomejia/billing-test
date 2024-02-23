49965d17a885e043bccd205263082f1a3def6765
jacastanomejia <isokmiox@outlook.com>
2024-02-23 11:39:32 -0500

    Documentar controlador de productos para ser visualizado con swagger

--- billing/src/main/java/com/magnetron/billing/controller/ProductRestController.java
+++ billing/src/main/java/com/magnetron/billing/controller/ProductRestController.java
@@ -6,6 +6,10 @@ import com.magnetron.billing.enumeration.StatusMessage;
 import com.magnetron.billing.service.contract.IProductService;
 import com.magnetron.billing.service.dto.ProductDto;
 import com.magnetron.billing.util.MessageParse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,19 +26,57 @@ public class ProductRestController {
 
 	@Autowired
 	private IProductService service;
-	
+
+	@Operation(
+			summary = "Obtener todos los productos en base de datos",
+			description = "Recuperar la lista existente de productos en base de datos"
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Lista de productos recuperada con éxito",
+					content = @Content(mediaType = "application/json")),
+	})
 	@GetMapping
 	public ResponseEntity<Object> getAll(){
 		List<ProductDto> dtoList  = service.getAll();
 		return MessageParse.generateResponse(StatusMessage.OK.toString(), HttpStatus.OK, dtoList);
 	}
-	
+
+	@Operation(
+			summary = "Obtener un producto por su ID",
+			description = "Recuperar un producto específico por su identificador único"
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Producto recuperado con éxito",
+					content = @Content(mediaType = "application/json")),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Producto no encontrado",
+					content = @Content),
+	})
 	@GetMapping(path="/{id}")
 	public ResponseEntity<ProductDto> getById(@PathVariable Long id){
 		ProductDto dto = service.getById(id);
 		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
 
+	@Operation(
+			summary = "Crear un nuevo producto",
+			description = "Crear un nuevo producto con los datos proporcionados"
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "201",
+					description = "Producto creado con éxito",
+					content = @Content(mediaType = "application/json")),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Datos de entrada inválidos",
+					content = @Content),
+	})
 	@PostMapping
 	public ResponseEntity<Object> create(@Valid @RequestBody ProductDto data, BindingResult validation) {
 		// Check validations defined in ProductDto
@@ -46,6 +88,24 @@ public class ProductRestController {
 		return MessageParse.generateResponse(StatusMessage.OK.toString(), HttpStatus.CREATED, result);
 	}
 
+	@Operation(
+			summary = "Actualizar un producto existente por su ID",
+			description = "Actualizar un producto existente con los nuevos datos proporcionados"
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "201",
+					description = "Producto actualizado con éxito",
+					content = @Content(mediaType = "application/json")),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Datos de entrada inválidos",
+					content = @Content),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Producto no encontrado",
+					content = @Content),
+	})
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Object> update(@PathVariable Long id, @Valid @RequestBody ProductDto data, BindingResult validation) {
  		if(validation.hasErrors()) {
@@ -55,7 +115,21 @@ public class ProductRestController {
 		ProductDto result = service.update(id, data);
 		return MessageParse.generateResponse(StatusMessage.OK.toString(), HttpStatus.CREATED, result);
 	}
-	
+
+	@Operation(
+			summary = "Eliminar un producto por su ID",
+			description = "Eliminar un producto existente por su identificador único"
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Producto eliminado con éxito",
+					content = @Content(mediaType = "application/json")),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Producto no encontrado",
+					content = @Content),
+	})
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Object> delete(@PathVariable Long id) {
 		boolean result = service.delete(id);

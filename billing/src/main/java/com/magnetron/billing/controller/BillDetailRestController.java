e68f22eb05810e733181cfcd4e3c740fe657523f
jacastanomejia <isokmiox@outlook.com>
2024-02-23 18:36:59 -0500

    Ajustar documentación de controlador de detalle de factura

--- billing/src/main/java/com/magnetron/billing/controller/BillDetailRestController.java
+++ billing/src/main/java/com/magnetron/billing/controller/BillDetailRestController.java
@@ -8,7 +8,9 @@ import com.magnetron.billing.service.dto.BillDetailDto;
 import com.magnetron.billing.service.dto.BillDetailFullDto;
 import com.magnetron.billing.util.MessageParse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
@@ -36,7 +38,7 @@ public class BillDetailRestController {
 			@ApiResponse(
 					responseCode = "200",
 					description = "Lista de detalles de factura recuperada con éxito",
-					content = @Content(mediaType = "application/json")),
+					content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = BillDetailDto.class))))
 	})
 	@GetMapping
 	public ResponseEntity<Object> getAll(@PathVariable Long id){
@@ -45,15 +47,47 @@ public class BillDetailRestController {
 	}
 
 	@Operation(
-			summary = "Crear nueva linea en detalle de factura",
-			description = "Permite el almacenamiento de una linea con detalle de la compra"
+			summary = "Obtener una línea del detalle de la factura por ID",
+			description = "Recupera una línea del detalle de la factura por su ID."
 	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Línea del detalle de factura recuperada con éxito",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = BillDetailFullDto.class))),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Línea del detalle de factura no encontrada",
+					content = @Content)
+	})
 	@GetMapping(path="/{detailId}")
 	public ResponseEntity<BillDetailFullDto> getById(@PathVariable Long id, @PathVariable Long detailId){
 		BillDetailFullDto dto = service.getById(detailId);
 		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
 
+	@Operation(
+			summary = "Crear nueva línea en el detalle de la factura",
+			description = "Permite el almacenamiento de una línea con detalle de la compra."
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "201",
+					description = "Línea del detalle de factura creada con éxito",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = BillDetailDto.class))),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Error de validación de datos",
+					content = @Content),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Factura no encontrada",
+					content = @Content),
+			@ApiResponse(
+					responseCode = "422",
+					description = "Error de validación de datos",
+					content = @Content)
+	})
 	@PostMapping
 	public ResponseEntity<Object> create(@PathVariable Long id, @Valid @RequestBody BillDetailDto data, BindingResult validation) {
 		// Check validations defined in BillDetailDto
@@ -66,6 +100,28 @@ public class BillDetailRestController {
 		return MessageParse.generateResponse(StatusMessage.OK.toString(), HttpStatus.CREATED, result);
 	}
 
+	@Operation(
+			summary = "Actualizar una línea en el detalle de la factura por ID",
+			description = "Actualiza una línea en el detalle de la factura por su ID con los nuevos datos proporcionados."
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "201",
+					description = "Línea del detalle de factura actualizada con éxito",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = BillDetailDto.class))),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Error de validación de datos",
+					content = @Content),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Línea del detalle de factura no encontrada",
+					content = @Content),
+			@ApiResponse(
+					responseCode = "422",
+					description = "Error de validación de datos",
+					content = @Content)
+	})
 	@PutMapping(value = "/{detailId}")
 	public ResponseEntity<Object> update(@PathVariable Long id, @PathVariable Long detailId, @Valid @RequestBody BillDetailDto data, BindingResult validation) {
  		if(validation.hasErrors()) {
@@ -75,7 +131,21 @@ public class BillDetailRestController {
 		BillDetailDto result = service.update(detailId, data);
 		return MessageParse.generateResponse(StatusMessage.OK.toString(), HttpStatus.CREATED, result);
 	}
-	
+
+	@Operation(
+			summary = "Eliminar una línea en el detalle de la factura por ID",
+			description = "Elimina una línea en el detalle de la factura por su ID."
+	)
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Línea del detalle de factura eliminada con éxito",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = Boolean.class))),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Línea del detalle de factura no encontrada",
+					content = @Content)
+	})
 	@DeleteMapping(value = "/{detailId}")
 	public ResponseEntity<Object> delete(@PathVariable Long id, @PathVariable Long detailId) {
 		boolean result = service.delete(detailId);
